5b7e957b5fb7b9f1633636722e071520acb6708a
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2024-07-09 23:11:21 +0300

    add project files

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const Page = (): ReactElement => {
+  return <p>Customers Page</p>;
+}
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const Page = (): ReactElement => {
+  return <p>Invoices Page</p>;
+}
+
+export default Page;
